8ef36488425df604ff65675d42d383e52ab94e9a
Greg <greg@new-host.home>
2018-12-04 23:33:20 -0500

    Fixed hosting on S3

--- greg-site/src/app/app-routing.module.ts
+++ greg-site/src/app/app-routing.module.ts
@@ -5,9 +5,7 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent},
-  {path: 'about', component: AboutComponent},
-  {path: '', redirectTo: '/home', pathMatch: 'full'}
+  {path: '**', redirectTo: '', pathMatch: 'full'}
 ]
 
 @NgModule({
--- greg-site/src/app/app.component.ts
+++ greg-site/src/app/app.component.ts
@@ -6,6 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  navLinks = [{label: 'Home', path: '/home'},{label: 'About', path: '/about'}]
-  title = 'greg-site';
+  title = 'Gregs-Site';
 }
